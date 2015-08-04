ea10a82dab339dd81233231f157fc402c0040c13
mkao006 <mkao006@gmail.com>
2015-08-04 09:16:58 -0600

    corrected conversion from degree to radian

--- playground/generateExplorerDestination.js
+++ playground/generateExplorerDestination.js
@@ -3,31 +3,30 @@ function generateExplorerDestination(){
     var r = 6367449;
 
     //pre-set the distance in meters
-    var distance = 1000000;
+    var distance = 100000;
     
     var angularDistance = distance/r;
     
     //Extract the home location coordinates
-    var oldLat = homeLocation.lat() * Math.PI/100;
-    var oldLng = homeLocation.lng() * Math.PI/100;
+    var oldLat = homeLocation.lat() * Math.PI/180;
+    var oldLng = homeLocation.lng() * Math.PI/180;
 
     //generate radom bearing
     var bearing = Math.random() * 2 * Math.PI;
     // var bearing = Math.random() * 360;
 
-    //Calculate new location. There is definitely comething wrong with
-    //the calculation here!
+    //Calculate new location
     var latShift = Math.asin(Math.sin(oldLat) * Math.cos(angularDistance) +
 			     Math.cos(oldLat) * Math.sin(angularDistance) *
 			     Math.cos(bearing));
-    var newLat = (latShift)/Math.PI * 100;
+    var newLat = (latShift)/Math.PI * 180;
     var lngShift = Math.atan2(Math.sin(bearing) * 
 			      Math.sin(angularDistance) *
 			      Math.cos(oldLat),
 			      Math.cos(angularDistance) - 
 			      Math.sin(oldLat) * 
 			      Math.sin(newLat));
-    var newLng = (oldLng + lngShift)/Math.PI * 100;
+    var newLng = (oldLng + lngShift)/Math.PI * 180;
     console.log(latShift);
     console.log(lngShift);
     console.log(newLat);

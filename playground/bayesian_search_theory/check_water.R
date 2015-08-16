581b9a7846937e9709ea448fd1938bfa886d821b
mk <mkao006@gmail.com>
2015-08-16 09:57:47 -0600

    First commit of the function to check water body

---
+++ playground/bayesian_search_theory/check_water.R
@@ -0,0 +1,6 @@
+########################################################################
+## Title: This is an implementation of how to check whether a given
+## location is part of a water body or not.
+## Date: 2015-08-07
+########################################################################
+

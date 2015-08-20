14adf4fc8b16869a395c293e22c943fcabadf3b5
mk <mkao006@gmail.com>
2015-08-20 13:18:57 -0600

    finished part 3

--- playground/django_test/random_walker/polls/views.py
+++ playground/django_test/random_walker/polls/views.py
@@ -1,22 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
-from .models import Question
 from django.template import RequestContext, loader
+from .models import Question
 
 # Create your views here.
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return HttpResponse("You're looking at question %s." % question_id)
+#     NOTE (Michael): This is only available if stubmethods does not exist!
+#     return render(request, 'polls/detail.html', {'question': question})
+
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return HttpResponse("You're looking at question %s." % question_id)
-    # NOTE (Michael): This is only available if stubmethods does not exist!
-    # return render(request, 'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):

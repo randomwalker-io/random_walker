fef994cb248b3b04b144ec06cfb34ae8901359d3
mk <mkao006@gmail.com>
2015-09-06 15:06:05 -0600

    set up static folder for js and python scripts

---
+++ playground/django_test/js_python_ajax_test/generateNewDestination/static/generateNewDestination/js_script/initialise.js
@@ -0,0 +1,68 @@
+function initialize() {
+    //Set home location as Auckland if failed to to geolocate
+    homeLocation = new google.maps.LatLng(-36.857992, 174.7621796);
+    
+    var mapOptions = {
+	zoom: 8,
+	center: homeLocation
+    }
+    map = new google.maps.Map(document.getElementById('map'),  
+			      mapOptions);
+
+    // Try HTML5 geolocation
+    if(navigator.geolocation) {
+    	navigator.geolocation.getCurrentPosition(function(position) {
+    	    homeLocation = new google.maps.LatLng(position.coords.latitude,
+    						  position.coords.longitude);
+	    
+    	    var infowindow = new google.maps.InfoWindow({
+    		map: map,
+    		position: homeLocation,
+    		content: 'Location found using HTML5.'
+    	    });
+	    
+    	    map.setCenter(homeLocation);
+    	}, function() {
+    	    handleNoGeolocation(true);
+    	});
+    } else {
+    	// Browser doesn't support Geolocation
+    	handleNoGeolocation(false);
+    }
+
+    function handleNoGeolocation(errorFlag) {
+    	if (errorFlag) {
+    	    var content = 'Error: The Geolocation service failed.';
+    	} else {
+    	    var content = 'Error: Your browser doesn\'t support geolocation.';
+    	}
+    	var options = {
+    	    map: map,
+    	    position: homeLocation,
+    	    content: content
+    	};
+
+    	var infowindow = new google.maps.InfoWindow(options);
+    	map.setCenter(options.position);
+    };
+}
+
+function addMarker(location) {
+    var mapOptions = {
+    	zoom: 7,
+    	center: homeLocation
+    };
+
+    // Access the map object 
+    var map = new google.maps.Map(document.getElementById("map"), 
+				  mapOptions);
+    
+    // Create the marker
+    var marker = new google.maps.Marker({
+	position: location,
+	map: map
+    })
+    
+    // Push the marker to the map
+    marker.push(marker);
+}
--- playground/django_test/js_python_ajax_test/js_python_ajax_test/urls.py
+++ playground/django_test/js_python_ajax_test/js_python_ajax_test/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^generateNewDestination/', include('generateNewDestination.urls', namespace='generateNewDestination')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 

a99b79dd2afac78f82eda7f10602a51a3d83f283
mk <mkao006@gmail.com>
2015-09-13 14:05:13 -0600

    added raw functions for updating layers

--- playground/django_test/js_python_ajax_test/generateNewDestination/static/generateNewDestination/py_script/definePriorLayer.py
+++ playground/django_test/js_python_ajax_test/generateNewDestination/static/generateNewDestination/py_script/definePriorLayer.py
@@ -6,22 +6,29 @@ import matplotlib.pyplot as plt
 def calcEucDist(x, y, center):
     return np.sqrt((x - center[0])**2 + (y - center[1])**2)
 
-def calcPixIndex(locx, locy, locnw, locse, resx, resy):
-    pixelx = np.ceil((locx.astype(float) - locnw[0])/locse[0] * resx).tolist()
-    pixely = np.ceil((locy.astype(float) - locnw[1])/locse[1] * resy).tolist()
+def coordToPixel(loc, locnw, locse, resx, resy):
+    pixelx = np.ceil((loc[0].astype(float) - locnw[0])/(locse[0] - locnw[0]) * resx).tolist()
+    pixely = np.ceil((loc[1].astype(float) - locnw[1])/(locse[1] - locnw[1]) * resy).tolist()
     return zip(pixelx, pixely)
 
-def createLayerIndex(resx, resy):
+def pixelToCoord(pixels, locnw, locse, resx, resy):
+    pixelx, pixely = zip(*pixels)
+    indx = pixelx/resx * (locse[0] - lownw[0]) + locnw[0]
+    indy = pixely/resy * (locse[1] - lownw[1]) + locnw[1]
+    return np.array([indx, indy])
+    
+
+def createIndexLayer(resx, resy):
     indx = np.tile(np.arange(resx) + 1, resx)
     indy = np.array([val for val in np.arange(resy) + 1 for _ in np.arange(resy)])
     return np.array([indx, indy])
 
-def definePriorLayer(sx, sy):
-    indx = np.tile(np.arange(sx), sx)
-    indy = np.array([val for val in np.arange(sy) for _ in np.arange(sy)])
-    distLayer = calcEucDist(indx, indy, [sx/2, sy/2]).reshape(sx, sy)
-    maxDist = np.sqrt((sx/2)**2 + (sy/2)**2)
-    distr = scipy.stats.norm(sx/4, maxDist/3)
+def definePriorLayer(indx, indy):
+    dimx = indx.max()
+    dimy = indy.max()
+    distLayer = calcEucDist(indx, indy, [dimx/2, dimy/2]).reshape(dimx, dimy)
+    maxDist = np.sqrt((dimx/2)**2 + (dimy/2)**2)
+    distr = scipy.stats.norm(maxDist/3, maxDist/3)
     unnormalisedLayer = distr.pdf(distLayer)
     normalisedLayer = unnormalisedLayer/unnormalisedLayer.sum()
     return normalisedLayer
@@ -43,21 +50,21 @@ def createLearningLayer(indx, indy, center):
 
 
 plot = True
-
-layer = createLayerIndex(1280, 1280)
-ind = calcPixIndex(np.random.random(20) * 3,  np.random.random(20) * 3, [0, 0], [3, 3], 1280, 1280)
+nLearningPoint = 5
+layer = createIndexLayer(1280, 1280)
+ind = coordToPixel(np.array([np.random.random(nLearningPoint),  np.random.random(nLearningPoint)]), [0, 0], [1, 1], 1280, 1280)
 learningLayer = createLearningLayer(layer[0], layer[1], ind)
 if(plot):
     plt.imshow(learningLayer)
     plt.show()
 
-probLayer = definePriorLayer(1280, 1280)
+probLayer = definePriorLayer(layer[0], layer[1])
 if(plot):
     plt.imshow(probLayer)
     plt.show()
 
 ## source the get water script
-waterLayer = getIsWater([-36.857992, 174.7621796], 7)
+waterLayer = getIsWater([47.8922573,3.0057252], 5)
 
 ## Create the posterio layer
 postLayer = probLayer * learningLayer
@@ -69,8 +76,14 @@ if(plot):
 finalLayer = probLayer * waterLayer * learningLayer
 if(plot):
     plt.imshow(finalLayer)
+    plt.colorbar()
     plt.show()
 
+sampleLayer = finalLayer/finalLayer.sum()
 
 im = Image.fromarray(np.array(finalLayer/finalLayer.max() * 255, dtype="uint8"))
 im.save("test.png")
+
+
+newIndex = np.random.choice(range(1280 * 1280), 1, p=sampleLayer.flatten().tolist())
+

7e111ca1649e4ee05df58b4372a80ab07079e92c
mk <mkao006@gmail.com>
2015-12-28 12:27:08 +0800

    added implementation of the feasible layer

--- playground/prob_layer_class/ProbLayer.py
+++ playground/prob_layer_class/ProbLayer.py
@@ -1,5 +1,8 @@
 import numpy as np
 import scipy.stats
+import cStringIO
+from PIL import Image
+import urllib
 
 ## Define a grid class
 
@@ -14,9 +17,12 @@ class Grid(object):
         self.lng = np.tile(np.linspace(bounds['southWest']['lng'], 
                                        bounds['northEast']['lng'], 
                                        size['lng']), size['lat'])
+
     def createPriorLayer(self, bandwidth):
         '''Method for creating the prior layer
-           NOTE (Michael): The probability will depends on the kernel type.'''
+        
+        NOTE (Michael): The probability will depends on the kernel type.
+        '''
         distr = scipy.stats.norm(0, bandwidth)
         dist = np.sqrt((self.lat - self.center['lat'])**2 + 
                        (self.lng - self.center['lng'])**2)
@@ -25,7 +31,8 @@ class Grid(object):
         return normalisedLayer.reshape(self.size.values())
 
     def createLearningLayer(self, kernelType, bandwidth, learningPoints):
-        '''Method for creating the learning layer'''
+        '''Method for creating the learning layer
+        '''
         positions = np.vstack([self.lng, self.lat])
         values = np.vstack([learningPoints['lng'], learningPoints['lat']])
         kernel = scipy.stats.gaussian_kde(values, bw_method = bandwidth)
@@ -33,15 +40,35 @@ class Grid(object):
         normalisedLayer = unnormalisedLayer/unnormalisedLayer.sum()
         return normalisedLayer.reshape(self.size.values())
 
-    # ## Define a method for creating feasible layer
-    # ##
-    # ## NOTE (Michael): input can be either an image of the same dimension
-    # ##                 or a polygon.
-    # def createFeasibleLayer(self, image, polygon):
-    #     ''' Method for creating the feasible layer'''
+    ## Define a method for creating feasible layer
+    ##
+    ## NOTE (Michael): input can be either an image of the same dimension
+    ##                 or a polygon.
+    def createFeasibleLayer(self, zoom):
+        ''' Method for creating the feasible layer'''
+        url = 'https://api.mapbox.com/v4/mkao006.cierjexrn01naw0kmftpx3z1h/' +  str(self.center['lng']) + ',' + str(self.center['lat']) + ',' + str(zoom) + '/' + str(self.size['lng']) + 'x' + str(self.size['lat']) + '.jpg?access_token=pk.eyJ1IjoibWthbzAwNiIsImEiOiJjaWVyamV5MnkwMXFtOXRrdHRwdGw4cTd0In0.H28itS1jvRgLZI3JhirtZg'
+        print url
+        file = cStringIO.StringIO(urllib.urlopen(url).read())
+        img = Image.open(file)
+        r = np.array(img.getdata(band=0)).reshape(512, 1024)
+        g = np.array(img.getdata(band=1)).reshape(512, 1024)
+        b = np.array(img.getdata(band=2)).reshape(512, 1024)
+        feasibleLayer = (((abs(r - 115) < 30) * (abs(g - 181) < 30) * (abs(b - 229) < 50)) == False) * 1
+        # feasibleLayer = (((r == 115) * (g == 181) * (b == 229)) == False) * 1
+        return feasibleLayer
 
-    # ## Define a method for creating bias layer
-    # ##
-    # def createBiasLayer(self, kernelType, bandwidth, biasPoints):
+    ## Define a method for creating bias layer
+    ##
+    def createBiasLayer(self, kernelType, bandwidth, biasPoints):
+        ''' Method for creating the bias layer
+        
+        Essentially the method is identical to the learning layer
+        '''
+        positions = np.vstack([self.lng, self.lat])
+        values = np.vstack([biasPoints['lng'], biasPoints['lat']])
+        kernel = scipy.stats.gaussian_kde(values, bw_method = bandwidth)
+        unnormalisedLayer = np.reshape(kernel(positions), self.size.values())
+        normalisedLayer = unnormalisedLayer/unnormalisedLayer.sum()
+        return normalisedLayer.reshape(self.size.values())
 
 
--- playground/prob_layer_class/ProbLayerTest.py
+++ playground/prob_layer_class/ProbLayerTest.py
@@ -3,23 +3,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 nRand = 50
+zoom = 10
+## Center in Auckland
 center = {'lat': -36.85764758564406, 'lng': 174.76226806640625}
+## Center in Taipei 
+# center = {'lat': 25.0483397, 'lng': 121.5375121}
 bounds = {'southWest': {'lat': -37.13842453422676, 'lng': 174.05914306640625}, 
           'northEast': {'lat': -36.57583533849175, 'lng': 175.46539306640625}}
 size = {'lat': 512, 'lng': 1024}
-learningPoints = {'lat': np.random.normal(center['lat'], 0.05, nRand),
-                  'lng': np.random.normal(center['lng'], 0.05, nRand)}
+learningPoints = {'lat': np.random.uniform(bounds['northEast']['lat'], 
+                                           bounds['southWest']['lat'], nRand),
+                  'lng': np.random.uniform(bounds['northEast']['lng'], 
+                                           bounds['southWest']['lng'], nRand)}
 
 ## Create new grid
 newGrid = pl.Grid(center, bounds, size)
 
 ## Create prior layer
-priorLayer = newGrid.createPriorLayer(0.1)
+priorLayer = newGrid.createPriorLayer(20)
 plt.imshow(priorLayer)
 plt.show()
 
 ## Create Learning layer
-learningLayer = newGrid.createLearningLayer('normal', 0.5, learningPoints)
+learningLayer = newGrid.createLearningLayer('normal', 0.3, learningPoints)
 ymax, ymin, xmax, xmin = bounds['northEast']['lat'], bounds['southWest']['lat'], bounds['northEast']['lng'], bounds['southWest']['lng']
 
 fig = plt.figure()
@@ -30,3 +36,17 @@ ax.plot(learningPoints['lng'], learningPoints['lat'], 'k.', markersize=10)
 ax.set_xlim([xmin, xmax])
 ax.set_ylim([ymin, ymax])
 plt.show()
+
+
+## Create Feasible Layer
+feasibleLayer = newGrid.createFeasibleLayer(zoom)
+plt.imshow(feasibleLayer)
+plt.show()
+
+
+## Create final Layer
+unFinalLayer = priorLayer * learningLayer * feasibleLayer
+normFinalLayer = unFinalLayer/unFinalLayer.sum()
+plt.imshow(normFinalLayer)
+plt.show()
+

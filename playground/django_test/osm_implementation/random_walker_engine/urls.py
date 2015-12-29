554eb7139c60b5381bf1b538238b537c8dd38c76
mk <mkao006@gmail.com>
2015-12-29 17:22:48 +0800

    Changed name for consistency

--- playground/django_test/osm_implementation/random_walker_engine/urls.py
+++ playground/django_test/osm_implementation/random_walker_engine/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.engine_home, name='engine_home'),
+    url(r'^$', views.index, name='index'),
 ]
--- playground/django_test/osm_implementation/random_walker_engine/views.py
+++ playground/django_test/osm_implementation/random_walker_engine/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render, render_to_response
 # Create your views here.
 from django.http import HttpResponse
 
-def engine_home(request):
+def index(request):
     return render_to_response('random_walker_engine/random_walker_engine.html')

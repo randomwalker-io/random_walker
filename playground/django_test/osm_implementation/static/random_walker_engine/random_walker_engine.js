56a0388aa7307b462d06a84212824cac235ac15d
mk <mkao006@gmail.com>
2015-12-29 18:05:31 +0800

    First commit of the static folder

---
+++ playground/django_test/osm_implementation/static/random_walker_engine/random_walker_engine.js
@@ -0,0 +1,10 @@
+var map = L.map('map').setView([-36.857992, 174.7621796], 10);
+
+// Mapbox tile layer
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    // maxZoom: 5,
+    id: 'mkao006.cierjexrn01naw0kmftpx3z1h',
+    accessToken: 'pk.eyJ1IjoibWthbzAwNiIsImEiOiJjaWVyamV5MnkwMXFtOXRrdHRwdGw4cTd0In0.H28itS1jvRgLZI3JhirtZg',
+    tileSize: 256
+}).addTo(map);

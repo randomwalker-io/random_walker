fd98866ad980024b12f2653a675d0148256f8a94
mk <mkao006@gmail.com>
2015-12-30 00:43:59 +0800

    successfully implemented the ajax with artificial points added to the existing map

--- playground/django_test/osm_implementation/random_walker_engine/urls.py
+++ playground/django_test/osm_implementation/random_walker_engine/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'newDestination', views.newDestination, name='newDestination'),
 ]
--- playground/django_test/osm_implementation/random_walker_engine/views.py
+++ playground/django_test/osm_implementation/random_walker_engine/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+import json
 
 # Create your views here.
-from django.http import HttpResponse
+
 
 def index(request):
     return render_to_response('random_walker_engine/random_walker_engine.html')
+
+def newDestination(request):
+    return HttpResponse(json.dumps((0, 0)), content_type="application/json")
+

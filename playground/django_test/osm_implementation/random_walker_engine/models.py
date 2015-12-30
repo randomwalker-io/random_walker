6cbbeb286479bf64d81a5946cbcb32091271e699
mk <mkao006@gmail.com>
2015-12-30 20:38:01 +0800

    implemented the data base and the model, but untested

--- playground/django_test/osm_implementation/random_walker_engine/models.py
+++ playground/django_test/osm_implementation/random_walker_engine/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    # Probably better to change the user id according to the following link
+    #
+    # http://stackoverflow.com/questions/2672975/django-biginteger-auto-increment-field-as-primary-key
+    user_id = models.BigIntegerField()
+    user_first_name = models.CharField(max_length=200)
+    user_last_name = models.CharField(max_length=200)
+    user_email = models.CharField(max_length=200)
+    user_address = models.CharField(max_length=200)
+    user_gender = models.CharField(max_length=20)
+    password_salt = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    user_date_registration = models.DateTimeField()
+    
+
+class Location(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    origin_lat = models.FloatField()
+    origin_lng = models.FloatField()
+    destination_lat = models.FloatField()
+    destination_lng = models.FloatField()
+    date_generation = models.DateTimeField()
--- playground/django_test/osm_implementation/random_walker_engine/views.py
+++ playground/django_test/osm_implementation/random_walker_engine/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import json
 import ProbLayer as pl
 import numpy as np
+import models
 # Create your views here.
 
 
@@ -12,9 +13,12 @@ def index(request):
 def newDestination(request):
     # NOTE (Michael): Change back to POST when the database is setup
     if request.method == 'POST':
-        json_data = json.loads(request.body)
-
+        # initialisation
         nRand = 50
+        user_id = 1
+
+        # Load data and create the Grid class
+        json_data = json.loads(request.body)
         zoom = json_data['zoom']
         center = {'lat': json_data['lat'], 'lng': json_data['lng']}
 
@@ -24,15 +28,34 @@ def newDestination(request):
                                 'lng': json_data['boundne']['lng']}}
         size = {'lat': json_data['size']['y'], 'lng': json_data['size']['x']}
         # size = {'lat': 512, 'lng': 1024}
-        learningPoints = {'lat': np.random.uniform(bounds['northEast']['lat'],
-                                                   bounds['southWest']['lat'], nRand),
-                          'lng': np.random.uniform(bounds['northEast']['lng'],
-                                                   bounds['southWest']['lng'], nRand)}
-        ## Create new grid
+        # learningPoints = {'lat': np.random.uniform(bounds['northEast']['lat'],
+        #                                            bounds['southWest']['lat'], nRand),
+        #                   'lng': np.random.uniform(bounds['northEast']['lng'],
+        #                                            bounds['southWest']['lng'], nRand)}
+        learningPoints = getPriorDestination(user_id)
         newGrid = pl.Grid(center, bounds, size, zoom)
+        
+        # Create the layers
         priorLayer = pl.createPriorLayer(newGrid, 20)
         learningLayer = pl.createLearningLayer(newGrid, 'normal', 0.3, learningPoints)
         feasibleLayer = pl.createFeasibleLayer(newGrid)
         finalLayer = priorLayer * learningLayer * feasibleLayer
-        return HttpResponse(json.dumps(finalLayer.sample()), content_type="application/json")
+        newDestination = finalLayer.sample()
+        saveNewDestination(user_id, origin=center, new_destination=newDestination)
+        return HttpResponse(json.dumps(newDestination), content_type="application/json")
 
+
+def getPriorDestination(id):
+    user = User.objects.get(id=id)
+    lat = user.location_set.values_list('destination_lat', flat=True)
+    lng = user.location_set.values_list('destination_lng', flat=True)
+    return {'lat': lat, 'lng': lng}
+
+def saveNewDestination(id, origin, new_destination):
+    user = User.objects.get(id=id)
+    user.location_set.create(
+        origin_lat = origin[0],
+        origin_lng = origin[1],
+        destination_lat = new_destination[0],
+        destination_lng = new_destination[1]
+    )

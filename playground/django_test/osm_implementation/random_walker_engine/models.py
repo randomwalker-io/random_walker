ff591296556b85c86ef32206235fbf8da1199144
mk <mkao006@gmail.com>
2015-12-31 08:01:31 +0800

    changed field type

--- playground/django_test/osm_implementation/random_walker_engine/models.py
+++ playground/django_test/osm_implementation/random_walker_engine/models.py
@@ -18,8 +18,8 @@ class User(models.Model):
 
 class Location(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    origin_lat = models.FloatField()
-    origin_lng = models.FloatField()
-    destination_lat = models.FloatField()
-    destination_lng = models.FloatField()
+    origin_lat = models.DecimalField(max_digits=20, decimal_places=10)
+    origin_lng = models.DecimalField(max_digits=20, decimal_places=10)
+    destination_lat = models.DecimalField(max_digits=20, decimal_places=10)
+    destination_lng = models.DecimalField(max_digits=20, decimal_places=10)
     date_generation = models.DateTimeField()

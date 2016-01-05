79471701b104b2bbb6c7a2fdf51cd937f3143de5
mk <mkao006@gmail.com>
2016-01-05 21:39:23 +0800

    removed redudant tests

--- playground/prob_layer_class/ProbLayerTest.py
+++ playground/prob_layer_class/ProbLayerTest.py
@@ -12,25 +12,16 @@ center = {'lat': -36.85764758564406, 'lng': 174.76226806640625}
 bounds = {'southWest': {'lat': -37.13842453422676, 'lng': 174.05914306640625}, 
           'northEast': {'lat': -36.57583533849175, 'lng': 175.46539306640625}}
 size = {'lat': 512, 'lng': 1024}
-# learningPoints = {'lat': np.random.uniform(bounds['northEast']['lat'],
-#                                            bounds['southWest']['lat'], nRand),
-#                   'lng': np.random.uniform(bounds['northEast']['lng'],
-#                                            bounds['southWest']['lng'], nRand)}
-# learningPoints = {'lat': [],
-#                   'lng': []}
-# learningPoints = {'lat': [Decimal('-37.13842453422676'), Decimal('-36.57583533849175')],
-#                   'lng': [Decimal('174.05914306640625'), Decimal('175.46539306640625')]}
+learningPoints = {'lat': np.random.uniform(bounds['northEast']['lat'],
+                                           bounds['southWest']['lat'], nRand),
+                  'lng': np.random.uniform(bounds['northEast']['lng'],
+                                           bounds['southWest']['lng'], nRand)}
 
-# learningPoints = {'lat': [-37.1384, -36.5758],
-#                   'lng': [174.0591, 175.4653]}
-
-learningPoints = {'lat': [],
-                  'lng': []}
 ## Create new grid
 newGrid = pl.Grid(center, bounds, size, zoom)
 
 ## Create prior layer
-priorLayer = pl.createPriorLayer(newGrid, 0.3)
+priorLayer = pl.createPriorLayer(newGrid, 1)
 plt.imshow(priorLayer.probLayer)
 plt.show()
 

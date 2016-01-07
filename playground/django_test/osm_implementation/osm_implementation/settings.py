0c28356e886ba2387716f518af3b8649131a6e3f
mk <mkao006@gmail.com>
2016-01-07 10:56:09 +0800

    corrected processor for template

--- playground/django_test/osm_implementation/osm_implementation/settings.py
+++ playground/django_test/osm_implementation/osm_implementation/settings.py
@@ -63,8 +63,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'home',
-                'random_walker_engine',
+                'django.core.context_processors.csrf',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',

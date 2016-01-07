30adc80c19193eb5036379fa3a6a26130548fd0d
mk <mkao006@gmail.com>
2016-01-07 10:58:53 +0800

    added success page after sign up

--- playground/django_test/osm_implementation/user_profile/urls.py
+++ playground/django_test/osm_implementation/user_profile/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^sign_up/$', views.sign_up, name='sign_up')
+    url(r'^sign_up/$', views.sign_up, name='sign_up'),
+    url(r'^create_user', views.create_user, name='create_user'),
 ]
--- playground/django_test/osm_implementation/user_profile/views.py
+++ playground/django_test/osm_implementation/user_profile/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render, render_to_response
+from django.views.decorators.csrf import csrf_protect, csrf_exempt, requires_csrf_token
+from django.template import RequestContext
 
 # Create your views here.
 
 def sign_up(request):
-    return render_to_response('user_profile/sign_up.html')
+    return render_to_response('user_profile/sign_up.html', context_instance=RequestContext(request))
+
+@requires_csrf_token
+def create_user(request):
+    # if request.method == 'POST':
+    # return render(request, 'user_profile/sign_up_successful.html', {})
+    return render_to_response('user_profile/sign_up_successful.html')
+    # u = User.objects.create_user(
+    #     username = username,
+    #     password = password,
+    #     email = "mkao006@gmail.com",
+    #     first_name = "Michael",
+    #     last_name = "Kao"
+    # )
+    # u.save()
+    # NOTE (Michael): We will create the extended profile later
+    #
+    # u.userprofile(
+    #     address = "my home",
+    #     gender = "M",
+    #     date_registration = timezone.now()
+    # )

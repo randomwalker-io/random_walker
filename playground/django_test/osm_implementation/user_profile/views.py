8704fd377752b51b6586841d209ddc1c47aef19e
mk <mkao006@gmail.com>
2016-01-07 16:57:09 +0800

    successfully implemented the create user functionality with values saved in database

--- playground/django_test/osm_implementation/user_profile/views.py
+++ playground/django_test/osm_implementation/user_profile/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, render_to_response
 from django.views.decorators.csrf import csrf_protect, csrf_exempt, requires_csrf_token
 from django.template import RequestContext
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -9,17 +10,22 @@ def sign_up(request):
 
 @requires_csrf_token
 def create_user(request):
-    # if request.method == 'POST':
-    # return render(request, 'user_profile/sign_up_successful.html', {})
-    return render_to_response('user_profile/sign_up_successful.html')
-    # u = User.objects.create_user(
-    #     username = username,
-    #     password = password,
-    #     email = "mkao006@gmail.com",
-    #     first_name = "Michael",
-    #     last_name = "Kao"
-    # )
-    # u.save()
+    if request.method == 'POST':
+        username = request.POST.get('Username', '')
+        password = request.POST.get('Password', '')
+        first_name = request.POST.get('First_name', '')
+        last_name = request.POST.get('Last_name', '')
+        email = request.POST.get('Email', '')
+        print "New user " + username + " created! with pass word as " + password
+    u = User.objects.create_user(
+        username = username,
+        password = password,
+        email = email,
+        first_name = first_name,
+        last_name = last_name
+    )
+    u.save()
+    return render_to_response("home/index.html")
     # NOTE (Michael): We will create the extended profile later
     #
     # u.userprofile(

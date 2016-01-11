5bfc726fe7cd08a0172816038f1169a4ddfef90c
mk <mkao006@gmail.com>
2016-01-11 15:39:51 +0800

    removed the backslash, apparently this caused the link to be dysfunctional

--- playground/django_test/osm_implementation/random_walker_engine/urls.py
+++ playground/django_test/osm_implementation/random_walker_engine/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'newDestination/', views.newDestination, name='newDestination'),
+    url(r'newDestination', views.newDestination, name='newDestination'),
 ]

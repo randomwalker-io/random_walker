5f1849bd97bfc00e4397767c03eee44d0b179d8e
mk <mkao006@gmail.com>
2016-01-20 17:05:58 +0800

    added databse setting

--- random_walker/settings.py
+++ random_walker/settings.py
@@ -145,3 +145,9 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] =  dj_database_url.config()
+
+# Enable Persistent Connections
+DATABASES['default']['CONN_MAX_AGE'] = 500

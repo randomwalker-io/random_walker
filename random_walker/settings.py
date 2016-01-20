c1dbaacf74854486437e0c008a1861d245d6f234
mk <mkao006@gmail.com>
2016-01-20 17:07:24 +0800

    added dj_database_url package

--- random_walker/settings.py
+++ random_walker/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import socket
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 

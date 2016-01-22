df5a6e906411d43c6b37d7cd0ae84968e50ed4f7
mk <mkao006@gmail.com>
2016-01-22 13:14:41 +0800

    updated static and also added missing image for the startup theme

---
+++ static/random_walker/js/csrf.507f90cdcf54.js
@@ -0,0 +1,62 @@
+// source: https://docs.djangoproject.com/en/1.8/ref/csrf/
+
+// This function gets cookie with a given name
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+/*
+  The functions below will create a header with csrftoken
+*/
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+/* Sets the X-CSRFToken header for every jQuery ajax non-GET request
+to make CSRF protection easy. This fixes the example from Django docs
+here: https://docs.djangoproject.com/en/dev/ref/contrib/csrf/#ajax
+Raw 
+
+Source:
+http://stackoverflow.com/questions/5100539/django-csrf-check-failing-with-an-ajax-post-request/5107878#5107878
+
+*/
+
+$.ajaxSetup({ 
+     beforeSend: function(xhr, settings) {
+         function getCookie(name) {
+             var cookieValue = null;
+             if (document.cookie && document.cookie != '') {
+                 var cookies = document.cookie.split(';');
+                 for (var i = 0; i < cookies.length; i++) {
+                     var cookie = jQuery.trim(cookies[i]);
+                     // Does this cookie string begin with the name we want?
+                     if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                         break;
+                     }
+                 }
+             }
+             return cookieValue;
+         }
+         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+             // Only send the token to relative URLs i.e. locally.
+             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+         }
+     } 
+});

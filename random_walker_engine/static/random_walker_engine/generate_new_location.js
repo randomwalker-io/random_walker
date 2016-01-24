45f8bb76ba06d317dc1a2677981136bac534498d
mk <mkao006@gmail.com>
2016-01-24 13:42:41 +0800

    changed circle size to be a function of the zoom level

--- random_walker_engine/static/random_walker_engine/generate_new_location.js
+++ random_walker_engine/static/random_walker_engine/generate_new_location.js
@@ -15,11 +15,13 @@ $(function() {
 		console.log("successful")
 		if(typeof circle !== 'undefined')
 		    map.removeLayer(circle)
-		circle = L.circle(data, 500, {
-		    color: 'red',
-		    fillColor: '#f03',
-		    fillOpacity: 0.5
-		}).addTo(map);
+		circle = L.circle(data, 
+				  500 * Math.pow(2, 13 - map.getZoom()),
+				  {
+				      color: 'red',
+				      fillColor: '#f03',
+				      fillOpacity: 0.5
+				  }).addTo(map);
             },
             fail: function(data){
 		console.log("failed")

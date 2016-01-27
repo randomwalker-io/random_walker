73d8f5be24f0627a76e18824ed6f844146d79f5e
mk <mkao006@gmail.com>
2016-01-27 17:45:06 +0800

    added user profile page

--- registration/views.py
+++ registration/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django_mobile import get_flavour
+from geojson import Feature, Point, FeatureCollection
 
 # Create your views here.
 
@@ -73,3 +74,30 @@ def logout_view(request):
     if request.user.is_authenticated():
         logout(request)
         return HttpResponseRedirect('/')
+
+## This is duplicated in the views.py in random_walker_engine
+def getPriorDestination(username):
+    if not User.objects.filter(username=username).exists():
+        # NOTE (Michael): Here we assume the user is already
+        #                 created. Otherwise, we should redirect them
+        #                 to the sign-up page
+        #
+        # createUser(username)
+        print "User does not exist"
+    user = User.objects.get(username=username)
+    lat = user.location_set.values_list('destination_lat', flat=True)
+    lng = user.location_set.values_list('destination_lng', flat=True)
+    return {'lat': lat, 'lng': lng}
+
+
+def profile_view(request, username):
+    previous_points = getPriorDestination(username)    
+    points_json = [Point(x) for x in zip(previous_points['lng'], previous_points['lat'])]
+    feature_collection_json = FeatureCollection([Feature(geometry = x) for x in points_json])
+    center = {'lat': max(previous_points['lat']) - 
+              (max(previous_points['lat']) - min(previous_points['lat']))/2,
+              'lng': max(previous_points['lng']) - 
+              (max(previous_points['lng']) - min(previous_points['lng']))/2}
+    ## Remove the hard coding of the zoom, also check why there is a home in Panama!!!
+    url = 'https://api.mapbox.com/v4/mkao006.cierjexrn01naw0kmftpx3z1h/geojson(' + str(feature_collection_json) + ")/" +  str(center['lng']) + "," + str(center['lat']) + ',3/1280x512.jpg?access_token=pk.eyJ1IjoibWthbzAwNiIsImEiOiJjaWVyamV5MnkwMXFtOXRrdHRwdGw4cTd0In0.H28itS1jvRgLZI3JhirtZg'
+    return render(request, 'registration/_profile.html', context={'img': url})

806fb9f9c077f8ad1c24ff173858241d062dcc61
mk <mkao006@gmail.com>
2016-03-03 14:48:29 +0800

    added UserProfile model and the gender field in the profile page

--- user_action/models.py
+++ user_action/models.py
@@ -4,3 +4,11 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    gender_choice = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('', 'Unspecified'),
+    )
+    gender = models.CharField(max_length=2, choices = gender_choice, default='')

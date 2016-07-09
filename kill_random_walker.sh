193657c431cec5642714ea805e08ed4023c4e291
mk <mkao006@gmail.com>
2016-07-09 13:30:14 -0700

    remove sudo previlege in pipe as it is not needed

--- kill_random_walker.sh
+++ kill_random_walker.sh
@@ -3,5 +3,5 @@
 # kill the deployment, the image name should probably be passed
 imgName="random_walker_docker_single"
 
-sudo docker ps | grep $imgName | awk '{print $1}' | xargs sudo docker kill
+sudo docker ps | grep $imgName | awk '{print $1}' | xargs docker kill
 

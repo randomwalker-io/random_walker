e3c0259f3d4085c81b3b79eb3dc918d152888462
mk <mkao006@gmail.com>
2016-07-09 16:34:41 -0700

    removed security settings on local

--- web/settings/local_docker_single.py
+++ web/settings/local_docker_single.py
@@ -136,18 +136,18 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 LOGIN_URL = os.path.join(BASE_DIR, 'registration/login_view/')
 
 
-# Security settings for production
-SESSION_COOKIE_HTTPONLY = False
-CSRF_COOKIE_HTTPONLY = False
-SECURE_SSL_REDIRECT = True
-SECURE_SSL_HOST = True
-SECURE_HSTS_SECONDS = False
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_BROWSER_XSS_FILTER = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-X_FRAME_OPTIONS = 'DENY'
+# # Security settings for production
+# SESSION_COOKIE_HTTPONLY = False
+# CSRF_COOKIE_HTTPONLY = False
+# SECURE_SSL_REDIRECT = True
+# SECURE_SSL_HOST = True
+# SECURE_HSTS_SECONDS = False
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+# SECURE_BROWSER_XSS_FILTER = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# X_FRAME_OPTIONS = 'DENY'
 
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

2003f5d2162f1f97edc05267d668b132f2a71fda
mk <mkao006@gmail.com>
2016-07-14 16:11:22 -0700

    correct if else statement

--- kill_random_walker.sh
+++ kill_random_walker.sh
@@ -2,11 +2,12 @@
 
 # kill the deployment, the image name should probably be passed
 appName="random_walker"
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$GIT_BRANCH" = "dev" ]
 then
     echo "The server already stopped"
-elif [ "$GIT_BRANCH" = "master"]
+elif [ "$GIT_BRANCH" = "master" ]
 then
     sudo docker ps | grep $appName | awk "{print $1}" | xargs docker kill
 elif [ "$GIT_BRANCH" = "production" ]

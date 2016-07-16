cfcd056234184d1980ead8deb24472142846ce84
mk <mkao006@gmail.com>
2016-07-15 23:52:35 -0700

    removed components relating to postgres
    These component are now contained in the database Docker container.

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-echo Starting postgres
-exec start-stop-daemon --start --chuid postgres:postgres \
-     --exec /usr/lib/postgresql/9.3/bin/postgres -- \
-     -D /var/lib/postgresql/9.3/main \
-     -c config_file=/etc/postgresql/9.3/main/postgresql.conf &
 
 ## Remove copied migrations
 rm -r random_walker_engine/migrations/*

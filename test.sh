30c076469c60e7708234f09bdd395aafe8fd158d
mk <mkao006@gmail.com>
2016-08-19 14:45:32 -0700

    removed build and detach image

--- test.sh
+++ test.sh
@@ -19,13 +19,15 @@ kill `lsof -t -i :80`
 sh build.sh
 
 ## Start up docker compose
-docker-compose stop
+
 if [ "$GIT_BRANCH" = "dev" ];
 then
-    docker-compose -f docker-compose-dev.yml build
-    docker-compose -f docker-compose-dev.yml up
+    docker-compose -f docker-compose-dev.yml stop
+    docker-compose -f docker-compose-dev.yml rm -f
+    docker-compose -f docker-compose-dev.yml up -d
 elif [ "$GIT_BRANCH" = "master" ];
 then
-    docker-compose build
-    docker-compose up
+    docker-compose stop
+    docker-compose rm -f
+    docker-compose up -d
 fi

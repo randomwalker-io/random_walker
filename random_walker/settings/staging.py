477bb1fdcdbad32bdee65cb7b7408cd81248a6da
mk <mkao006@gmail.com>
2016-08-19 16:51:48 -0700

    remove redudant database chunk,
    The database configuration are now the same, but uses different env file.

--- random_walker/settings/staging.py
+++ random_walker/settings/staging.py
@@ -2,14 +2,3 @@
 ## than local.
 
 from .base_extended import *
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': env("RDS_DB_NAME"),
-        'USER': env("RDS_DB_USERNAME"),
-        'PASSWORD': env("RDS_DB_PASSWORD"),
-        'HOST': "postgis",
-        "PORT": 5432,
-    }
-}

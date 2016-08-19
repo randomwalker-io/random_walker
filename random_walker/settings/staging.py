050ff9ee1695c72e8159ce89989c4d89bd8fe17e
mk <mkao006@gmail.com>
2016-08-19 14:41:22 -0700

    changed reading secrets from fileto envrionmennt variable

--- random_walker/settings/staging.py
+++ random_walker/settings/staging.py
@@ -6,9 +6,9 @@ from .base_extended import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': get_secret("RDS_DB_NAME"),
-        'USER': get_secret("RDS_DB_USERNAME"),
-        'PASSWORD': get_secret("RDS_DB_PASSWORD"),
+        'NAME': env("RDS_DB_NAME"),
+        'USER': env("RDS_DB_USERNAME"),
+        'PASSWORD': env("RDS_DB_PASSWORD"),
         'HOST': "postgis",
         "PORT": 5432,
     }

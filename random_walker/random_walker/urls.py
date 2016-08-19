c1e9e4a3384534900be798d6afd7ebb1a2927578
mk <mkao006@gmail.com>
2016-08-19 14:46:21 -0700

    implement from django cookiecutter

--- random_walker/random_walker/urls.py
+++ random_walker/random_walker/urls.py
@@ -13,14 +13,51 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+
+# from django.conf.urls import include, url
+# from django.contrib import admin
+# from . import views
+
+# urlpatterns = [
+#     url('^$', views.index, name='index'),
+#     url(r'random_walker_engine/', include('random_walker_engine.urls', namespace='random_walker_engine')),
+#     url(r'user_action/', include('user_action.urls', namespace='user_action')),
+#     url(r'^shah_jahan/', admin.site.urls),
+
+# ]
+
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
-from . import views
+from django.views.generic import TemplateView
+from django.views import defaults as default_views
 
 urlpatterns = [
-    url('^$', views.index, name='index'),
-    url(r'random_walker_engine/', include('random_walker_engine.urls', namespace='random_walker_engine')),
-    url(r'user_action/', include('user_action.urls', namespace='user_action')),
-    url(r'^shah_jahan/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
-]
+    # Django Admin, use {% url 'admin:index' %}
+    url(settings.ADMIN_URL, include(admin.site.urls)),
+
+    # User management
+    url(r'^users/', include('users.urls', namespace='users')),
+    url(r'^accounts/', include('allauth.urls')),
+
+    # Your stuff: custom urls includes go here
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    # This allows the error pages to be debugged during development, just visit
+    # these url in browser to see how these error pages look like.
+    urlpatterns += [
+        url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),
+        url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
+        url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
+        url(r'^500/$', default_views.server_error),
+    ]

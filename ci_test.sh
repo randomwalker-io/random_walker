3dcae79a1602745ed2272a310086c57e010dd719
mk <mkao006@gmail.com>
2016-08-21 17:50:29 -0700

    added requirement installation

--- ci_test.sh
+++ ci_test.sh
@@ -3,6 +3,7 @@
 ## Run unit tests on the Random Walker image
 echo "Performing Unit Testing on the Rand Walker Django App ..."
 cd random_walker
+pip install -r requirement.txt
 python manage.py test
 cd ..
 

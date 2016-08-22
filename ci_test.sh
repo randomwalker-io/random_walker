52297047a634f53778a3e4a05565a242b953f386
mk <mkao006@gmail.com>
2016-08-21 18:21:20 -0700

    corrected file name

--- ci_test.sh
+++ ci_test.sh
@@ -3,7 +3,7 @@
 ## Run unit tests on the Random Walker image
 echo "Performing Unit Testing on the Rand Walker Django App ..."
 cd random_walker
-pip install -r requirement.txt
+pip install -r requirements.txt
 python manage.py test
 cd ..
 

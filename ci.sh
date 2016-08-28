0a536d6d6a36299a968dc7e428fe90fc490d6551
mk <mkao006@gmail.com>
2016-08-27 21:14:14 -0700

    test 5 times before failing the server test
    The reason for this is because the server need to wait for the containers
    to start up, otherwise it fails automatically

--- ci.sh
+++ ci.sh
@@ -10,15 +10,19 @@ cd ..
 ## Check status
 ##
 ## NOTE (Michael): Might want to elaborate this section to test all pages.
-echo "Testing home page accessibility ... "
-code=$(curl -s -o /dev/null -w "%{http_code}" localhost:80)
-echo $code
 
-if [ $code = 200 ];
-then
-    echo "Server launched successfully"
-    exit 0
-else
-    echo "Server launch failed"
-    exit 1
-fi
+echo "Testing home page accessibility ... "
+for i in {1..5}
+do
+    code=$(curl -s -o /dev/null -w "%{http_code}" localhost:80)
+    echo "Status Code:"$code
+    if [ $code = 200 ];
+    then
+        echo "Server launched successfully"
+        exit 0
+    fi
+    sleep 30s
+done;
+
+echo "Server launch failed"
+exit 1

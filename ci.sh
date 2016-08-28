fa54bccad5c5765312197c4f5f5d715f7e9b8f69
mk <mkao006@gmail.com>
2016-08-27 22:50:22 -0700

    removed page accessibility test

--- ci.sh
+++ ci.sh
@@ -6,28 +6,3 @@ cd random_walker
 pip install -r requirements.txt
 python manage.py test
 cd ..
-
-## Check status
-##
-## NOTE (Michael): Might want to elaborate this section to test all pages.
-
-echo "Testing home page accessibility ... "
-for i in {1..5}
-do
-    code=$(curl -s -o /dev/null -w "%{http_code}" localhost:80)
-    if [ $code = 200 ];
-    then
-        break
-    fi
-    sleep 30s
-done;
-
-echo "Status Code:"$code
-if [ $code = 200 ];
-then
-    echo "Server launched successfully"
-    exit 0
-else
-    echo "Server launch failed"
-    exit 1
-fi

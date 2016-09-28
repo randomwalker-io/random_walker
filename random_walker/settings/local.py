33ab3a0ac6e27b23622261f619e2ebabc495e8e1
mk <mkao006@gmail.com>
2016-09-28 13:21:47 +0800

    merged local compose and local

--- random_walker/settings/local.py
+++ random_walker/settings/local.py
@@ -10,7 +10,7 @@ DATABASES = {
         'NAME': env("DB_NAME"),
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASSWORD"),
-        'HOST': 'localhost',
+        'HOST': env("DB_HOST"),
         "PORT": env("DB_PORT"),
         "ATOMIC_REQUESTS": True
     }

d7924972429dc71f4afc0864558f604ac86b7f48
mk <mkao006@gmail.com>
2016-09-29 17:17:34 +0800

    merged local and base

--- random_walker/settings/base.py
+++ random_walker/settings/base.py
@@ -106,6 +106,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'random_walker.wsgi.application'
 
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        "PORT": env("DB_PORT"),
+        "ATOMIC_REQUESTS": True
+    }
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
